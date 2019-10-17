3359bdc0e4c11ba7de6a25127082a770f846650a
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-17 10:13:32 -0600

    Change state of form when inputs are entered

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -4,17 +4,25 @@ class Form extends Component {
   constructor() {
     super();
     this.state = {
-
+      customer: '',
+      date: '',
+      time: '',
+      guests: ''
     }
   }
 
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value})
+     }
+
   render() {
+    console.log(this.state.date)
     return (
       <form>
-        <input placeholder="Name" type="text"/>
-        <input placeholder="Date" type="text" />
-        <input placeholder="Time" type="text"/>
-        <input placeholder="Number of Guests" type="number"/>
+        <input placeholder="Name" type="text" name="customer" onChange={this.handleChange}/>
+        <input placeholder="Date" type="text" name="date" onChange={this.handleChange}/>
+        <input placeholder="Time" type="text" name="time" onChange={this.handleChange}/>
+        <input placeholder="Number of Guests" type="number" name="guests" onChange={this.handleChange}/>
       </form>
     )
   }
