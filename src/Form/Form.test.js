a09134f52f084468724f0f8432612ba5cb56b8bb
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-17 11:37:50 -0600

    Add test to show change in state when a name is being inputted

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -14,30 +14,30 @@ describe('Form', () => {
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should update name when handleChange is being called', () => {
+    // Setup
+    const mockEvent = { 
+      target: { 
+        name: 'name', 
+        value: 'Travis'
+      }
+    };
+    const expected = 'Travis';
+  
+    // Expectation
+    expect(wrapper.state('name')).toEqual('');
+
+  
+    // Execution
+    wrapper.instance().handleChange(mockEvent);
+  
+    // Expectation
+    expect(wrapper.state('name')).toEqual(expected);
+  });
 });
 
 
 
 // describe('Form', () => {
-//   it('should update name when handleChange is being called', () => {
-//     // Setup
-//     const mockFn = jest.fn();
-//     const wrapper = shallow(<Form addReservation={mockFn}/>);
-//     const mockEvent = { 
-//       target: { 
-//         name: 'name', 
-//         value: 'Travis'
-//       }
-//     };
-//     const expected = 'Travis';
-
-//     // Expectation
-//     expect(wrapper.state('name')).toEqual('');
-
-//     // Execution
-//     wrapper.instance().handleChange(mockEvent);
-
-//     // Expectation
-//     expect(wrapper.state('ideas')).toEqual(expected);
-//   });
 // });
